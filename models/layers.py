d8562daa58156e4982503db040a04710e1ed3a3c
tian <t18215595124@gmail.com>
2020-02-01 01:13:54 +0800

    inception frn

--- models/layers.py
+++ models/layers.py
@@ -102,7 +102,7 @@ class noalpha(nn.Module):
         nn.init.ones_(self.gamma)
         nn.init.zeros_(self.beta)
         nn.init.zeros_(self.tau)
-        nn.init.zeros_(self.limit)
+        nn.init.constant_(self.limit,0.5)
         nn.init.constant_(self.eps,1e-4)
     def forward(self, x,start=0,end=1):
         """
