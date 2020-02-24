b7ea72410c2617d784dbf78600ed7779abc07a69
tian <t18215595124@gmail.com>
2020-02-24 11:31:23 +0800

    new bn

--- models/layers.py
+++ models/layers.py
@@ -213,14 +213,15 @@ class NewBatchNorm2d(nn.Module):
         if self.training:
             mean = x.mean(dim=(0, 2, 3), keepdim=True).to(x.device)
             var = (x - mean).pow(2).mean(dim=(0, 2, 3), keepdim=True).to(x.device)
+            self.running_mean = (self.momentum) * self.running_mean + (1-self.momentum) * mean
+
             self.running_var = (self.momentum) * self.running_var + (1 - self.momentum) * var
-            # var = torch.max(self.limit,var)
+            var = torch.max(self.limit,var)
             x = self.gamma * (x - mean) / torch.sqrt(var + self.eps) + self.beta
             # self.running_var = (self.momentum) * self.running_var + (1 - self.momentum) * var
 
-            self.running_mean = (self.momentum) * self.running_mean + (1-self.momentum) * mean
         else:
-
-            x = self.gamma * (x - self.running_mean) / (torch.sqrt(self.running_var +
+            var = torch.max(self.limit,self.running_var)
+            x = self.gamma * (x - self.running_mean) / (torch.sqrt(var +
                                                                    self.eps)) + self.beta
         return x
