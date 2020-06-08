eab2f0e58055ed22d2b5702c9efec0ed780412c4
T1anZhenYu <18215595124@163.com>
2020-06-08 13:42:44 +0800

    detach var keep maxmin

--- models/cifar/resnet_detachVarKeepMaxMinGrad.py
+++ models/cifar/resnet_detachVarKeepMaxMinGrad.py
@@ -9,9 +9,9 @@ https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
 '''
 import torch.nn as nn
 import math
-from ..layers import noalpha
+from ..layers import NewFilterResponseNormalization
 
-__all__ = ['resnet_noalpha']
+__all__ = ['newfrn_resnet']
 
 
 def conv3x3(in_planes, out_planes, stride=1):
@@ -34,10 +34,10 @@ class Bottleneck(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
-        self.frn1 = noalpha(planes)
-        self.frn2 = noalpha(planes)
-        self.frn3 = noalpha(planes * 4)
-        self.frn4 = noalpha(planes * 4)
+        self.frn1 = NewFilterResponseNormalization(planes)
+        self.frn2 = NewFilterResponseNormalization(planes)
+        self.frn3 = NewFilterResponseNormalization(planes * 4)
+        self.frn4 = NewFilterResponseNormalization(planes * 4)
 
     def forward(self, x):
         residual = x
@@ -77,11 +77,11 @@ class BasicBlock(nn.Module):
         self.downsample = downsample
         self.stride = stride
 
-        self.frn1 = noalpha(planes)
+        self.frn1 = NewFilterResponseNormalization(planes)
         # self.tlu1 = TLU(planes)
 
-        self.frn2 = noalpha(planes)
-        self.frn3 = noalpha(planes)
+        self.frn2 = NewFilterResponseNormalization(planes)
+        self.frn3 = NewFilterResponseNormalization(planes)
         # self.tlu2 = TLU(planes)
 
 
@@ -123,7 +123,7 @@ class ResNet_Frn(nn.Module):
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1,
                                bias=False)
         # self.bn1 = nn.BatchNorm2d(16)
-        self.frn1 = noalpha(16)
+        self.frn1 = NewFilterResponseNormalization(16)
         # self.tlu1 = TLU(16)
 
         self.relu = nn.ReLU(inplace=True)
@@ -137,7 +137,7 @@ class ResNet_Frn(nn.Module):
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, noalpha):
+            elif isinstance(m, NewFilterResponseNormalization):
                 m.gamma.data.fill_(1)
                 m.beta.data.zero_()
 
@@ -147,7 +147,7 @@ class ResNet_Frn(nn.Module):
             downsample = nn.Sequential(
                 nn.Conv2d(self.inplanes, planes * block.expansion,
                           kernel_size=1, stride=stride, bias=False),
-                # noalpha(planes * block.expansion),
+                # NewFilterResponseNormalization(planes * block.expansion),
             )
 
         layers = []
@@ -178,8 +178,9 @@ class ResNet_Frn(nn.Module):
         return x
 
 
-def resnet_noalpha(**kwargs):
+def newfrn_resnet(**kwargs):
     """
     Constructs a ResNet model.
     """
+    print("in newfrn_resnet")
     return ResNet_Frn(**kwargs)
