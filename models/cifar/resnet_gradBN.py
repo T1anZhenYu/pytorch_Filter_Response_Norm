a0b9f863ef5641266fff76f98759b5e6103260c4
tianzhenyu <18215595124@163.com>
2020-06-11 06:26:28 +0000

    model

--- models/cifar/resnet_gradBN.py
+++ models/cifar/resnet_gradBN.py
@@ -11,7 +11,7 @@ import torch.nn as nn
 import math
 
 
-__all__ = ['resnet']
+__all__ = ['gradbn_resnet']
 
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
@@ -160,7 +160,7 @@ class ResNet(nn.Module):
         return x
 
 
-def resnet(**kwargs):
+def gradbn_resnet(**kwargs):
     """
     Constructs a ResNet model.
     """
