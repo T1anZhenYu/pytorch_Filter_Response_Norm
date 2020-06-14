d56ad4027a4206d91acada573e7c19d16cf9b23b
T1anZhenYu <18215595124@163.com>
2020-06-14 12:17:38 +0800

    detachvar

--- models/cifar/__init__.py
+++ models/cifar/__init__.py
@@ -5,4 +5,5 @@ from .resnet import *
 from .resnet_oldfrn import *
 from .resnet_oldbn import *
 from .resnet_detachVarKeepMaxMinGrad import *
-from .resnet_gradBN import * 
+from .resnet_gradBN import *
+from .resnet_detachVar import *
--- models/layers.py
+++ models/layers.py
@@ -318,3 +318,78 @@ class GradBatchNorm(nn.BatchNorm2d):
             y = y * self.weight[None, :, None, None] + self.bias[None, :, None, None]
         y = y.float()
         return y
+
+
+class DetachVarFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, running_mean, running_var, eps, momentum):
+        n = x.numel() / (x.size(1))
+        # running_mean = running_mean.double()
+        # running_var = running_var.double()
+        mean = x.mean(dim=(0, 2, 3), keepdim=True)
+        # mean = torch.clamp(mean,min=0,max=4)
+        # print('mean size:', mean.size())
+        # use biased var in train
+
+        var = (x - mean).pow(2).sum(dim=(0, 2, 3)) / (n)
+        mean = mean.squeeze()
+        var = var.squeeze()
+
+        running_mean.copy_(momentum * mean \
+                           + (1 - momentum) * running_mean)
+        # update running_var with unbiased var
+        running_var.copy_(momentum * var * n / (n - 1) \
+                          + (1 - momentum) * running_var)
+        # running_mean = running_mean.float()
+        # running_var = running_var.float()
+        y = (x - mean[None, :, None, None]) / (torch.sqrt(var[None, :, None, None]))
+        ctx.eps = 0.00
+        ctx.save_for_backward(y, var, )
+        return y
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # print("grad dtype")
+
+        eps = ctx.eps
+        y, var = ctx.saved_variables
+        n = y.numel() / y.size(1)
+        # print('y dtype',y.dtype)
+        g = grad_output
+        # print("g:",g[:,0,:,:])
+        gy = (g * y).sum(dim=(0, 2, 3), keepdim=True) / (n) * y
+        # print("gy dtype",gy.dtype)
+        # print("g*y",(g * y).mean(dim=(0,2,3),keepdim=True)[:,0,:,:])
+        # print("gy:",gy[:,0,:,:])
+        g1 = g.mean(dim=(0, 2, 3), keepdim=True)
+        # print("g1:",g1[:,0,:,:])
+        gx_ = g - g1
+        # print("g - g1",(g-g1)[:,0,:,:])
+        # print("gx_:",gx_[:,0,:,:])
+        gx = 1. / torch.sqrt(var[None, :, None, None]) * (gx_)
+        # gx = gx.float()
+        # print("gx:",gx[:,0,:,:])
+        return gx, None, None, None, None
+
+
+class DetachVarNorm(nn.BatchNorm2d):
+    def __init__(self, num_features, eps=0.00001, momentum=0.1,
+                 affine=True, track_running_stats=True):
+        super(DetachVarNorm, self).__init__(
+            num_features, eps, momentum, affine, track_running_stats)
+
+    def forward(self, x):
+        self._check_input_dim(x)
+        self.running_mean = self.running_mean.double()
+        self.running_var = self.running_var.double()
+        x = x.double()
+        if self.training:
+            y = DetachVarFunction.apply(x, self.running_mean, self.running_var, self.eps, self.momentum)
+        else:
+            mean = self.running_mean
+            var = self.running_var
+            y = (x - mean[None, :, None, None]) / (torch.sqrt(var[None, :, None, None] + self.eps))
+        if self.affine:
+            y = y * self.weight[None, :, None, None] + self.bias[None, :, None, None]
+        y = y.float()
+        return y
