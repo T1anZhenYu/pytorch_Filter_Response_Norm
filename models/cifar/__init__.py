97e1b69e6780f08bb11bca81081520e99c0448f0
T1anZhenYu <18215595124@163.com>
2020-06-23 23:20:54 +0800

    desnet 121

--- models/cifar/__init__.py
+++ models/cifar/__init__.py
@@ -1,16 +1,7 @@
 from __future__ import absolute_import
 
 from .resnet import *
-from .resnet_oldfrn import *
-from .resnet_oldbn import *
-from .resnet_detachVarKeepMaxMinGrad import *
-from .resnet_gradBN import *
-from .resnet_detachVar import *
-from .resnet_detachVarKeepMaxMinGrad import *
-from .resnet_officialdetachVar import *
-from .resnet_rangeBN import *
-from .resnet_myRangeBN import *
-from .resnet_DetachClipRangeBN import *
-from .resnet_DetachClipRangeBNNoMean import *
+
 from .resnet_varlearn import *
-from .resnet_varfix import *
+
+from .densnet_varlearn import *
